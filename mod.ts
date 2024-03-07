4aeb4d9370b967248662080c2333fd595ff7d425
August Skare <post@augustskare.no>
2024-03-07 20:18:58 +0100

    Load .env file

--- mod.ts
+++ mod.ts
@@ -1,7 +1,9 @@
+import { load } from "dotenv";
 import { router } from "./utils/router.tsx";
 import * as feed from "./routes/feed.tsx";
 import * as index from "./routes/index.tsx";
 
+await load({ export: true });
 router([
   [{ pathname: "/" }, index],
   [{ pathname: "/:slug" }, feed],
